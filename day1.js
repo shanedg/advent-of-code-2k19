5d1c9a832b09b7b645c6aaead1b9392d848d7e4a
Shane Garrity <shanedgarrity@gmail.com>
2019-12-04 00:52:52 -0500

    complete day 1

--- day1.js
+++ day1.js
@@ -1,7 +1,53 @@
-const main = () => {
-  console.log('DAY 1');
+const fs = require('fs');
+const util = require('util');
 
+const readFilePromise = util.promisify(fs.readFile);
 
+const part1 = masses => {
+  console.log('Part 1:');
+  const fuelReqs = masses.reduce((counter, current) => {
+    if (current === '') {
+      return counter;
+    }
+    return counter + getFuelForMass(current);
+  }, 0);
+  return fuelReqs;
+};
+
+const part2 = masses => {
+  console.log('Part 2:');
+  let fuelForTheFuel = 0;
+  masses.forEach(value => {
+    if (value !== '') {
+      const mass = parseInt(value);
+      fuelForTheFuel += getRecursiveFuel(mass);
+    }
+  });
+  return fuelForTheFuel;
+}
+
+const getFuelForMass = mass => {
+  return Math.floor((parseInt(mass) / 3)) - 2;
+}
+
+const getRecursiveFuel = mass => {
+  const fuel = getFuelForMass(mass);
+  if (fuel <= 0) {
+    return 0;
+  }
+  return fuel + getRecursiveFuel(fuel);
+}
+
+const main = async () => {
+  console.log('## DAY 1 ##');
+
+  const rawInput = await readFilePromise('./day_1_input.txt', { encoding: 'utf-8' });
+  const masses = rawInput.split('\n');
+  const regularFuel = part1(masses);
+  console.log(regularFuel);
+
+  const fuelForTheFuel = part2(masses);
+  console.log(fuelForTheFuel);
 };
 
 main();
