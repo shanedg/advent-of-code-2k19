c2c6966e5b20f5e8680b9912af26fbe3267031cf
Shane Garrity <shanedgarrity@gmail.com>
2019-12-04 01:23:10 -0500

    complete part 1 of day 2

---
+++ day2.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFilePromise = util.promisify(fs.readFile);
+
+const part1 = intCodes => {
+  console.log('Part 1:');
+
+  let position = 0;
+  const codes = intCodes.map(code => parseInt(code));
+
+  codes[1] = 12;
+  codes[2] = 2;
+
+  while (codes[position] !== 99) {
+    const opCode = codes[position];
+
+    const param1Address = codes[position + 1];
+    const param2Address = codes[position + 2];
+    const resultAddress = codes[position + 3];
+
+    if (opCode === 1) {
+      codes[resultAddress] = codes[param1Address] + codes[param2Address];
+    } else if (opCode === 2) {
+      codes[resultAddress] = codes[param1Address] * codes[param2Address];
+    }
+
+    position += 4;
+  }
+
+  return codes[0];
+};
+
+const part2 = intCodes => {
+  console.log('Part 2:');
+};
+
+const getIntCodes = input => {
+  const splitOnNewline = input.split('\n');
+  return splitOnNewline[0].split(',');
+}
+
+const main = async () => {
+  console.log('## DAY 2 ##');
+
+  const rawInput = await readFilePromise('./day_2_input.txt', { encoding: 'utf-8' });
+  const intCodes = getIntCodes(rawInput);
+
+  const position0Value = part1(intCodes);
+  console.log(position0Value);
+};
+
+main();
