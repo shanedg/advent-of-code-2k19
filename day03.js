4304998d4840c65f958d2841cee56dadaef19c72
Shane Garrity <shanedgarrity@gmail.com>
2019-12-05 01:47:40 -0500

    start day 3, no parts complete

--- day03.js
+++ day03.js
@@ -4,19 +4,55 @@ const util = require('util');
 const readFilePromise = util.promisify(fs.readFile);
 const day = '03';
 
-const part1 = () => {
+const reduceCoordinates = (counter, current, index) => {
+  const partialCoordinates = [...counter];
+
+  const { x, y } = partialCoordinates[index];
+  const nextDirection = current.charAt(0);
+  const distance = parseInt(current.slice(1));
+
+  let newX = x;
+  let newY = y;
+  if (nextDirection === 'U' || nextDirection === 'D') {
+    newY = y + (nextDirection === 'U' ? distance : -distance);
+  } else {
+    newX = x + (nextDirection === 'R' ? distance : -distance);
+  }
+
+  const newCoordinates = {
+    x: newX,
+    y: newY,
+  }
+  partialCoordinates.push(newCoordinates);
+
+  return partialCoordinates;
+}
+
+const getCoordinates = wire => {
+  const origin = { x: 0, y: 0 };
+  return wire.reduce(reduceCoordinates, [ origin ]);
+}
+
+const part1 = (wire1, wire2) => {
   console.log('Part 1:');
+
+  const wire1Coordinates = getCoordinates(wire1);
+  // const wire2Coordiantes = getCoordinates(wire2);
+
+  console.log('wire1 coordinates', wire1Coordinates);
 };
 
-const part2 = () => {
-  console.log('Part 2:');
-};
+// const part2 = () => {
+//   console.log('Part 2:');
+// };
 
 const main = async () => {
   console.log(`## DAY ${day} ##`);
 
   const rawInput = await readFilePromise(`./day${day}_input.txt`, { encoding: 'utf-8' });
-  console.log(rawInput);
+  const [ wire1, wire2 ] = rawInput.split('\n').map(path => path.split(','));
+
+  part1(wire1, wire2);
 };
 
 main();
