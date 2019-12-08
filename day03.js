bbeab0b0b7cadfb0dbf6418ac1516ea67cc147ce
Shane Garrity <shanedgarrity@gmail.com>
2019-12-07 21:10:29 -0500

    solve day 3 part 1

--- day03.js
+++ day03.js
@@ -4,42 +4,78 @@ const util = require('util');
 const readFilePromise = util.promisify(fs.readFile);
 const day = '03';
 
-const reduceCoordinates = (counter, current, index) => {
-  const partialCoordinates = [...counter];
+const reduceCoordinates = (previousCoordinates, currentInstruction) => {
+  const { x, y } = previousCoordinates[previousCoordinates.length - 1];
 
-  const { x, y } = partialCoordinates[index];
-  const nextDirection = current.charAt(0);
-  const distance = parseInt(current.slice(1));
+  const direction = currentInstruction.charAt(0);
+  const distance = parseInt(currentInstruction.slice(1));
 
-  let newX = x;
-  let newY = y;
-  if (nextDirection === 'U' || nextDirection === 'D') {
-    newY = y + (nextDirection === 'U' ? distance : -distance);
-  } else {
-    newX = x + (nextDirection === 'R' ? distance : -distance);
+  const positive = direction === 'U' || direction === 'R';
+  const yAxis = direction === 'U' || direction === 'D';
+
+  for (let i = 1; i <= distance; i += 1) {
+    if (yAxis) {
+      previousCoordinates.push({
+        x: x,
+        // Parentheses are important here!
+        // Without them, x is coerced to a boolean before adding i!
+        y: y + (positive ? i : -i),
+      });
+    } else {
+      previousCoordinates.push({
+        // Parentheses are important here!
+        // Without them, x is coerced to a boolean before adding i!
+        x: x + (positive ? i : -i),
+        y: y,
+      });
+    }
   }
 
-  const newCoordinates = {
-    x: newX,
-    y: newY,
-  }
-  partialCoordinates.push(newCoordinates);
-
-  return partialCoordinates;
-}
+  return previousCoordinates;
+};
 
 const getCoordinates = wire => {
   const origin = { x: 0, y: 0 };
-  return wire.reduce(reduceCoordinates, [ origin ]);
+  return wire.reduce(reduceCoordinates, [origin]);
 }
+
+const findIntersections = (wire1, wire2) => {
+  const intersections = [];
+
+  wire1.forEach((w1, i) => {
+    if (w1.x === 0 && w1.y === 0) {
+      return;
+    }
+    const intersect = wire2.find(w2 => {
+      return w1.x === w2.x && w1.y === w2.y;
+    });
+    if (intersect) {
+      intersections.push(w1);
+    }
+  });
+
+  return intersections;
+};
+
+const manhattanDistance = (x, y) => Math.abs(x) + Math.abs(y);
+
+const reduceManhattanDistances = (shortestDistance, coordinates) => {
+  const distanceFromOrigin = manhattanDistance(coordinates.x, coordinates.y);
+  if (shortestDistance === 0 || distanceFromOrigin < shortestDistance) {
+    return distanceFromOrigin;
+  }
+  return shortestDistance;
+};
 
 const part1 = (wire1, wire2) => {
   console.log('Part 1:');
 
   const wire1Coordinates = getCoordinates(wire1);
-  // const wire2Coordiantes = getCoordinates(wire2);
+  const wire2Coordinates = getCoordinates(wire2);
+  const intersections = findIntersections(wire1Coordinates, wire2Coordinates);
+  const shortestManhattanDistance = intersections.reduce(reduceManhattanDistances, 0);
 
-  console.log('wire1 coordinates', wire1Coordinates);
+  return shortestManhattanDistance;
 };
 
 // const part2 = () => {
@@ -52,7 +88,9 @@ const main = async () => {
   const rawInput = await readFilePromise(`./day${day}_input.txt`, { encoding: 'utf-8' });
   const [ wire1, wire2 ] = rawInput.split('\n').map(path => path.split(','));
 
-  part1(wire1, wire2);
+  const shortestDistanceFromOrigin = part1(wire1, wire2);
+  console.log('shortest manhattan distance from origin', shortestDistanceFromOrigin);
+
 };
 
 main();
