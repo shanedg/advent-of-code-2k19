a6ad34c0882279ac86a844796a13d0296e98c882
Shane Garrity <shanedgarrity@gmail.com>
2019-12-14 16:29:59 -0500

    save work on day 6, part 2

--- day06.js
+++ day06.js
@@ -17,24 +17,24 @@ const search = (name, orbitMap, depth = 0) => {
     };
   }
 
+  const nextNode = {
+    name,
+    children,
+    depth,
+  };
+
   while (index !== -1 && orbitMap.length > 0) {
     const orbit = orbitMap.splice(index, 1)[0];
     const directOrbit = orbit.split(')')[1];
     const orbittedBy = search(directOrbit, orbitMap, (depth + 1));
     if (orbittedBy) {
+      orbittedBy.parent = nextNode;
       children.push(orbittedBy);
     }
     index = orbitMap.findIndex(o => orbitPattern.test(o));
   }
 
-  if (children.length > 0) {
-  }
-
-  return {
-    name,
-    children,
-    depth,
-  };
+  return nextNode;
 };
 
 const countOrbits = (node) => {
@@ -50,6 +50,27 @@ const countOrbits = (node) => {
   return total;
 };
 
+const findNode = (tree, name) => {
+  if (tree.name === name) {
+    return tree;
+  }
+
+  if (tree.children === null) {
+    return null;
+  }
+
+  return tree.children.reduce((accum, current) => {
+    if (accum === null) {
+      const foundNode = findNode(current, name);
+      if (foundNode) {
+        return foundNode;
+      }
+      return null;
+    }
+    return accum;
+  }, null);
+}
+
 const main = async () => {
   console.log(`## Day ${DAY} ## Part 1`);
 
@@ -63,6 +84,14 @@ const main = async () => {
   const totalOrbits = countOrbits(tree);
   console.log(`total number of direct and indrect orbits ${totalOrbits}`);
   console.assert(totalOrbits === 322508);
+
+  console.log('Part 2');
+  const santa = findNode(tree, 'SAN');
+  const you = findNode(tree, 'YOU');
+  console.log(`santa at ${santa.name}, depth ${santa.depth}, orbitting ${santa.parent.name}`);
+  console.log(`you at ${you.name}, depth ${you.depth}, orbitting ${you.parent.name}`);
+
+  // TODO: find nearest common ancestor of SANTA and YOU
 };
 
 main();
